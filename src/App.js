1b53d6012a39fd65213eb5efc54b5bcde8ccd4ee
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-09-21 23:28:22 +0600

    Create child component and pass data via props

--- src/App.js
+++ src/App.js
@@ -1,48 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
-import Countries from './components/Country/Countries';
-import Person from './components/Person/Person';
-import Header from './components/Header/Header';
+import Countries from './components/Countries/Countries';
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
+      {/* <Header></Header> */}
       <Countries></Countries>
-      <Person></Person>
-      <LoadCountries></LoadCountries>
+      {/* <Person></Person> */}
+      {/* <LoadCountries></LoadCountries> */}
     </div>
   );
 }
 
-function LoadCountries() {
-  const [countries, setCountries] = useState([]);
+// function LoadCountries() {
+//   const [countries, setCountries] = useState([]);
 
-  useEffect(() => {
-    fetch('https://restcountries.com/v3.1/all')
-      .then(res => res.json())
-      .then(data => setCountries(data))
-  }, [])
+//   useEffect(() => {
+//     fetch('https://restcountries.com/v3.1/all')
+//       .then(res => res.json())
+//       .then(data => setCountries(data))
+//   }, [])
 
-  return (
-    <div>
-      <h2>Visiting Every Country In the world...!</h2>
-      <h3>Available Country: {countries.length}</h3>
-      {
-        countries.map(country => <Country name={country.name.common} population={country.population}></Country>)
-      }
-    </div>
-  )
-}
+//   return (
+//     <div>
+//       <h2>Visiting Every Country In the world...!</h2>
+//       <h3>Available Country: {countries.length}</h3>
+//       {
+//         countries.map(country => <Country name={country.name.common} population={country.population}></Country>)
+//       }
+//     </div>
+//   )
+// }
 
-function Country(props) {
-  return (
-    <div>
-      <h3>Name : {props.name}</h3>
-      <p>Population : {props.population}</p>
-    </div>
-  )
-}
+// function Country(props) {
+//   return (
+//     <div>
+//       <h3>Name : {props.name}</h3>
+//       <p>Population : {props.population}</p>
+//     </div>
+//   )
+// }
 
 export default App;
---
+++ src/components/Country/Country.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Country.css'
+
+const Country = (props) => {
+    return (
+        <div className='country'>
+            <h3>Country Name : {props.name}</h3>
+            <p>Population : {props.population}</p>
+            <p>Area: {props.area}</p>
+        </div>
+    );
+};
+
+export default Country;
